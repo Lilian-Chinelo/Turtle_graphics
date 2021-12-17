faff6c1f26969ec8a9394a9ea7e512bae3442867
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-12-17 10:26:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen = Screen()
+screen.onkey(fun=move_forwards, key="w")
+screen.onkey(fun=move_backwards, key="s")
+screen.onkey(fun=turn_left, key="a")
+screen.onkey(fun=turn_right, key="d")
+screen.onkey(fun=clear, key="c")
+
+
+screen.listen()
+screen.exitonclick()
